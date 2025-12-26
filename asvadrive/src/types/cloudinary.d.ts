3b82ecd8201ac9529cea09ff23a7d73f02df4d62
Solomon Tolulope <kaysaki63@gmail.com>
2025-12-26 22:12:02 +0100

    Removed accidental folder from the school repo

--- asvadrive/src/types/cloudinary.d.ts
+++
@@ -1,7 +0,0 @@
-import { UploadApiOptions as CloudinaryUploadApiOptions } from "cloudinary";
-
-declare module 'cloudinary' {
-    interface UploadApiOptions extends CloudinaryUploadApiOptions{
-        asset_folder?: string;  // asset_folder hasn't been added as a property in the current node SDK
-    }
-}
